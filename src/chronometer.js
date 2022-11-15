4bee191de22e017f787bb94772011e5083992653
guille garcia <ggarciacardiel@gmail.com>
2022-11-15 22:14:49 +0100

    chrono completed - no bonus yet

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) {
+        printTimeCallback ();
+      }
+      this.currentTime = this.currentTime + 1;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
-  }
+    let minutes = Math.floor(this.currentTime / 60);
+    return minutes;
+  } 
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = Math.floor(this.currentTime % 60);
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    const valueStr = value.toString()
+    if (valueStr.length === 1) {
+      return `0${value}` 
+    } else {
+      return valueStr;
+    }
+  }; 
 
   stop() {
-    // ... your code goes here
+    this.intervalId = clearInterval((this.intervalId))
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return  `${minutes}:${seconds}`
   }
 }
+
